280a16079e4b99027d2518ec8d3ecd70c8a27a40
Duc Pham Le <44611780+ducphamle2@users.noreply.github.com>
2021-01-06 16:12:20 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # download the docker-compose & orai.env file
 
-curl -OL https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/docker-compose.yml
+curl -OL https://raw.githubusercontent.com/oraichain/oraichain-static-files-private/testnet-dev/docker-compose.yml
 
-curl -OL https://raw.githubusercontent.com/oraichain/oraichain-static-files/master/orai.env
+curl -OL https://raw.githubusercontent.com/oraichain/oraichain-static-files-private/testnet-dev/orai.dev.env
 
 # modify the orai.env name & content
